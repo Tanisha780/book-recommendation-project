ef78c7833002b8cb439e957d250d32c01f0c47dd
Tanisha780 <141040330+Tanisha780@users.noreply.github.com>
2023-08-01 21:11:17 +0530

    Update appp.py

--- appp.py
+++ appp.py
@@ -1,7 +1,6 @@
 import pickle
 import streamlit as st
 import numpy as np
-import sklearn
 
 st.header("Book Recommender System Using Machine Learning")
 model  = pickle.load(open('model.pkl','rb'))
